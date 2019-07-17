838d8e15b77da9af423f7d404b816692b07a6c76
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-17 22:03:41 +0800

    Update Launch.java

--- src/Main/Launch.java
+++ src/Main/Launch.java
@@ -1,1 +1,24 @@
+package Main;
 
+public class Launch {
+	
+	private final String title="Survive";
+	
+	private final int width=600;
+	
+	private final int height=600;
+	
+	public Window window;
+	
+	private Display display;
+	
+	public static void main(String[]args) {
+		new Launch();
+	}
+	
+	public Launch() {
+		window=new Window(title,width,height);
+		display=new Display(window);
+		display.start();
+	}
+}
