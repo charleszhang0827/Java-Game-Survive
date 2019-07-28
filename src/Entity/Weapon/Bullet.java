c325174d8c848aeb47cb6c5374a7ec11f8965247
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-28 22:42:45 +0800

    Update Bullet.java

--- src/Entity/Weapon/Bullet.java
+++ src/Entity/Weapon/Bullet.java
@@ -1,1 +1,36 @@
+package Entity.Weapon;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import Main.Display;
+
+public class Bullet extends Weapon {
+	
+	private double angle;
+	
+	private final Color color=Color.CYAN;
+	
+	public Bullet(Display display,int x,int y,int width,int height,int damage,int xVelocity,int yVelocity,double angle) {
+		super(display,x,y,width,height,damage,xVelocity,yVelocity);
+		this.angle=angle;
+	}
+	
+	public void tick() {
+		movement();
+	}
+	
+	public void render(Graphics2D g) {
+		g.setColor(color);
+		g.fillRect(x,y,width,height);
+	}
+	
+	private void movement() {
+		x+=(int)(Math.cos(angle)*xVelocity);
+		y+=(int)(Math.sin(angle)*yVelocity);
+		
+		if(x+width<0 || x>display.getWidth() || y+height<0 || y>display.getHeight()) {
+			display.getRunState().removeBullet(this);
+		}
+	}
+}
