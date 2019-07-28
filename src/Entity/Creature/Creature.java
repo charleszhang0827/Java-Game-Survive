4e403699d423352e13936de0ee2691afca88e169
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-28 22:47:32 +0800

    Update Creature.java

--- src/Entity/Creature/Creature.java
+++ src/Entity/Creature/Creature.java
@@ -1,1 +1,44 @@
+package Entity.Creature;
 
+import Entity.Entity;
+import Main.Display;
+
+public abstract class Creature extends Entity {
+	
+	protected int health;
+	
+	protected int xVelocity;
+	
+	protected int yVelocity;
+	
+	public Creature(Display display,int x,int y,int width,int height,int health,int xVelocity,int yVelocity) {
+		super(display,x,y,width,height);
+		this.health=health;
+		this.xVelocity=xVelocity;
+		this.yVelocity=yVelocity;
+	}
+	
+	public void setHealth(int health) {
+		this.health=health;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+	
+	public void setXVelocity(int xVelocity) {
+		this.xVelocity=xVelocity;
+	}
+	
+	public int getXVelocity() {
+		return xVelocity;
+	}
+	
+	public void setYVelocity(int yVelocity) {
+		this.yVelocity=yVelocity;
+	}
+	
+	public int getYVelocity() {
+		return yVelocity;
+	}
+}
