36a1f47810ec436102217a8ff9f97a630aa6f70c
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-28 22:43:54 +0800

    Update Weapon.java

--- src/Entity/Weapon/Weapon.java
+++ src/Entity/Weapon/Weapon.java
@@ -1,1 +1,44 @@
+package Entity.Weapon;
 
+import Entity.Entity;
+import Main.Display;
+
+public abstract class Weapon extends Entity {
+	
+	protected int damage;
+	
+	protected int xVelocity;
+	
+	protected int yVelocity;
+	
+	public Weapon(Display display,int x,int y,int width,int height,int damage,int xVelocity,int yVelocity) {
+		super(display,x,y,width,height);
+		this.damage=damage;
+		this.xVelocity=xVelocity;
+		this.yVelocity=yVelocity;
+	}
+	
+	public void setDamage(int damage) {
+		this.damage=damage;
+	}
+	
+	public int getDamage() {
+		return damage;
+	}
+	
+	public void setXVelocity(int xVelocity) {
+		this.xVelocity=xVelocity;
+	}
+	
+	public int getXVelocity() {
+		return xVelocity;
+	}
+	
+	public void setYVelocity(int yVelocity) {
+		this.yVelocity=yVelocity;
+	}
+	
+	public int getYVelocity() {
+		return yVelocity;
+	}
+}
