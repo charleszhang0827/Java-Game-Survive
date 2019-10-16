90973d8451f8c2331b4e608bd169f73988b2ef6b
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-15 21:07:02 -0700

    Update Creature.java

--- src/Entity/Creature/Creature.java
+++ src/Entity/Creature/Creature.java
@@ -1,1 +1,21 @@
+package Entity.Creature;
 
+import Entity.Entity;
+import Main.Display;
+
+public abstract class Creature extends Entity {
+	protected int health;
+	
+	public Creature(Display display,int x,int y,int health) {
+		super(display,x,y);
+		this.health=health;
+	}
+	
+	public void setHealth(int health) {
+		this.health=health;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+}
