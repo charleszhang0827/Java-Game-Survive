e3d43878e4e82e68d544467950f52384d164c91d
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-15 21:12:34 -0700

    Update Tickable.java

--- src/Interface/Tickable.java
+++ src/Interface/Tickable.java
@@ -1,1 +1,51 @@
+package Interface;
 
+import java.awt.Rectangle;
+
+import Entity.Creature.Player;
+import Enum.Action;
+import Main.Display;
+import State.State;
+
+public interface Tickable {
+	public abstract void tick();
+	
+	public default void updateAction(Display display,Rectangle rectangle,Action action) {
+			
+		int mouseX=display.getMouseMotionHandler().getX();
+			
+		int mouseY=display.getMouseMotionHandler().getY();
+			
+		if(display.getMouseActionHandler().getPressed()) {
+			if(mouseX>rectangle.x && mouseX<rectangle.x+rectangle.width) {
+				if(mouseY>rectangle.y && mouseY<rectangle.y+rectangle.height) {
+					if(action==Action.start) {
+						State.setState(display.getStartState());
+					}else if(action==Action.help) {
+						State.setState(display.getHelpState());
+					}else if(action==Action.about) {
+						State.setState(display.getAboutState());
+					}else if(action==Action.run) {
+						State.setState(display.getRunState());
+					}else if(action==Action.end) {
+						display.resetEndState();
+						State.setState(display.getEndState());
+					}else if(action==Action.restart) {
+						display.resetRunState();
+						State.setState(display.getRunState());
+					}else if(action==Action.healthRefill) {
+						Player player=display.getRunState().getPlayer();
+						
+						if(player.getHealth()!=player.getHealthRange()) {
+							if(display.getRunState().getScore()>600) {
+								player.setHealth(player.getHealthRange());
+								display.getRunState().setScore(display.getRunState().getScore()-600);
+							}
+						}
+					}
+					display.resetTick();
+				}
+			}
+		}
+	}
+}
