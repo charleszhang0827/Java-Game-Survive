570dd8388dbb2681fee2b14dc09b922f493a34fc
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-15 21:08:53 -0700

    Update Weapon.java

--- src/Entity/Weapon/Weapon.java
+++ src/Entity/Weapon/Weapon.java
@@ -1,1 +1,21 @@
+package Entity.Weapon;
 
+import Entity.Entity;
+import Main.Display;
+
+public abstract class Weapon extends Entity {
+	protected int damage;
+	
+	public Weapon(Display display,int x,int y,int damage) {
+		super(display,x,y);
+		this.damage=damage;
+	}
+	
+	public void setDamage(int damage) {
+		this.damage=damage;
+	}
+	
+	public int getDamage() {
+		return damage;
+	}
+}
