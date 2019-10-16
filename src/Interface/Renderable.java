5d578d34809897aee57e476f402279ae01fc16b9
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-15 21:12:19 -0700

    Update Renderable.java

--- src/Interface/Renderable.java
+++ src/Interface/Renderable.java
@@ -1,1 +1,25 @@
+package Interface;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public interface Renderable {
+	public abstract void render(Graphics2D g);
+	
+	public default void drawCenterText(Graphics2D g,Rectangle rectangle,Font font,Color color,String text) {
+		FontMetrics fontMetrics=g.getFontMetrics(font);
+		
+		int x=rectangle.x+(rectangle.width-fontMetrics.stringWidth(text))/2;
+		
+		int y=rectangle.y+(rectangle.height- fontMetrics.getHeight())/2+fontMetrics.getAscent();
+		
+		g.setColor(color);
+		
+		g.setFont(font);
+		
+		g.drawString(text,x,y);
+	}
+}
