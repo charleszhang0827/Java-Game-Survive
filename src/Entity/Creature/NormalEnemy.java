a842583f8a252714336fba413d51888c39980281
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-15 21:07:19 -0700

    Update NormalEnemy.java

--- src/Entity/Creature/NormalEnemy.java
+++ src/Entity/Creature/NormalEnemy.java
@@ -1,1 +1,57 @@
+package Entity.Creature;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import Interface.Renderable;
+import Interface.Tickable;
+import Main.Display;
+
+public class NormalEnemy extends Creature implements Tickable,Renderable {
+	
+	public static final int NORMALENEMYSIZE=40;
+	
+	private int normalEnemyXVelocity=6;
+	
+	private int normalEnemyYVelocity=6;
+	
+	private final double angle=Math.random()*360;
+	
+	public NormalEnemy(Display display,int x,int y,int health) {
+		super(display,x,y,health);
+	}
+	
+	public void tick() {
+		move();
+		collideTick();
+	}
+	
+	private void move() {
+		x+=Math.cos(Math.toRadians(angle))*normalEnemyXVelocity;
+		y+=Math.sin(Math.toRadians(angle))*normalEnemyYVelocity;
+		
+		if(x<=0 || x+NORMALENEMYSIZE>=display.getWidth()) {
+			normalEnemyXVelocity=-normalEnemyXVelocity;
+		}
+		
+		if(y<=0 || y+NORMALENEMYSIZE>=display.getHeight()) {
+			normalEnemyYVelocity=-normalEnemyYVelocity;
+		}
+	}
+	
+	private void collideTick() {
+		Player player=display.getRunState().getPlayer();
+		if(x+NORMALENEMYSIZE>player.getX() && x<player.getX()+Player.PLAYERSIZE) {
+			if(y+NORMALENEMYSIZE>player.getY() && y<player.getY()+Player.PLAYERSIZE) {
+				player.setHealth(player.getHealth()-5);
+				display.getRunState().removeNormalEnemy(this);
+				return;
+			}
+		}
+	}
+	
+	public void render(Graphics2D g) {
+		g.setColor(Color.RED);
+		g.fillRect(x,y,NORMALENEMYSIZE,NORMALENEMYSIZE);
+	}
+}
