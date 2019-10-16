746097fac0aeea74b7a72d825162c5816f519a60
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-15 21:13:18 -0700

    Update Survive.java

--- src/Main/Survive.java
+++ src/Main/Survive.java
@@ -1,1 +1,37 @@
+package Main;
 
+import java.awt.Toolkit;
+
+public class Survive {
+	
+	private final String title="Survive";
+	
+	private int width;
+	
+	private int height;
+	
+	private Window window;
+	
+	private Display display;
+	
+	public static void main(String[]args) {
+		new Survive();
+	}
+	
+	private Survive() {
+		getDimension();
+		
+		window=new Window(title,width,height);
+		display=new Display(window);
+		display.start();
+	}
+	
+	private void getDimension() {
+		Toolkit toolKit=Toolkit.getDefaultToolkit();
+		
+		width=(int)(toolKit.getScreenSize().getWidth());
+		
+		height=(int)(toolKit.getScreenSize().getHeight());	
+	}
+	
+}
