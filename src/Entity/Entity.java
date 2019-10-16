3e6354375fe02e45ec9f040818d6c3f40f2f7406
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-10-15 21:06:23 -0700

    Update Entity.java

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -1,1 +1,33 @@
+package Entity;
 
+import Main.Display;
+
+public abstract class Entity {
+	protected Display display;
+	
+	protected int x;
+	
+	protected int y;
+	
+	public Entity(Display display,int x,int y) {
+		this.display=display;
+		this.x=x;
+		this.y=y;
+	}
+	
+	public void setX(int x) {
+		this.x=x;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setY(int y) {
+		this.y=y;
+	}
+	
+	public int getY() {
+		return y;
+	}
+}
